923970ed602e15d8dceecd00310748d850a91ebc
slavabdev <slav.balykov@gmail.com>
2021-05-25 23:06:32 +0000

    add sorting and counting of items

--- products/views.py
+++ products/views.py
@@ -21,6 +21,8 @@ def all_products(request):
             if sortkey == 'name':
                 sortkey = 'lower_name'
                 products = products.annotate(lower_name=Lower('name'))
+            if sortkey == 'category':
+                sortkey = 'category__name'
 
             if 'direction' in request.GET:
                 direction = request.GET['direction']
@@ -43,7 +45,7 @@ def all_products(request):
             queries = Q(name__icontains=query) | Q(description__icontains=query)
             products = products.filter(queries)
 
-    current_sorting = f'{sort}{direction}'
+    current_sorting = f'{sort}_{direction}'
 
     context = {
         'products': products,
