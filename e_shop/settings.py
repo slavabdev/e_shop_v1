e3742fbf52741a4c15a6c406e358caae57a90c4d
slavabdev <slav.balykov@gmail.com>
2021-06-11 00:32:32 +0000

    removed secret key

--- e_shop/settings.py
+++ e_shop/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&7t6&or%-ui6hz3^k*o)zjfyem_!gf1fa03et+z=-@=@%h8*mn'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['slavabdev-eshop.herokuapp.com', 'localhost']
 
@@ -123,8 +123,8 @@ LOGIN_REDIRECT_URL = '/'
 WSGI_APPLICATION = 'e_shop.wsgi.application'
 
 
-Database
-https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+# Database
+# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 if 'DATABASE_URL' in os.environ:
     DATABASES = {
