dd38397154d6db98a462f348f4a74ba986e1f2f8
slavabdev <slav.balykov@gmail.com>
2021-06-11 00:07:47 +0000

    deploying project

--- e_shop/settings.py
+++ e_shop/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-&7t6&or%-ui6hz3^k*o)zjfyem_!gf1fa03et+z=-@=@%h8*mn
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['slavabdev-eshop.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -126,12 +126,17 @@ WSGI_APPLICATION = 'e_shop.wsgi.application'
 Database
 https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
